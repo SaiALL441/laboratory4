8376600213469d63374db90999db28a66051f272
Александра <112776816+SaiALL441@users.noreply.github.com>
2022-10-23 20:58:14 +0300

    Add files via upload

---
+++ Ex3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+	class Transport
+	{
+		public float speed;
+	}
+
+	class Auto:Transport
+	{
+		private string model;
+
+		public Auto(float speed, string model)
+		{
+			this.speed = speed;
+			this.model = model;
+			Console.Write("Speed is " + speed + ", model is " + model);
+		}
+	}
+
+	class Program
+	{
+
+		public static void Main(String[] args)
+		{
+			Auto bmw = new Auto(250.5f, "X3");
+			Auto mercedes = new Auto(270f, "C63 AMG");
+		}
+	}
+}
